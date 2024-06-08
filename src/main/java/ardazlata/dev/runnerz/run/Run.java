9c48ce138557e08ac2152c9455d085139d4856ea
Arda Erdoğan <ardushka@outlook.com>
2024-06-08 04:30:30 +0200

    RestAPI is done. The next thing is to create a database and make them communicate with each other

--- src/main/java/ardazlata/dev/runnerz/run/Run.java
+++ src/main/java/ardazlata/dev/runnerz/run/Run.java
@@ -1,12 +1,24 @@
 package ardazlata.dev.runnerz.run;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
+
 import java.time.LocalDateTime;
 
 public record Run(
     Integer id,
+    @NotEmpty
     String title,
     LocalDateTime startedOn,
     LocalDateTime completedOn,
+    @Positive
     Integer miles,
     Location location
-) {}
+) {
+    public Run {
+        if (startedOn.isAfter(completedOn)) {
+            throw new IllegalArgumentException("The run cannot be completed before it has started.");
+        }
+    }
+
+}
--- src/main/java/ardazlata/dev/runnerz/run/RunController.java
+++ src/main/java/ardazlata/dev/runnerz/run/RunController.java
@@ -1,5 +1,6 @@
 package ardazlata.dev.runnerz.run;
 
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -30,7 +31,7 @@ public class RunController {
 
 
     @PostMapping("")
-    public ResponseEntity<Run> createRun(@RequestBody Run run) {
+    public ResponseEntity<Run> createRun(@Valid @RequestBody Run run) {
         runRepository.save(run);
         return new ResponseEntity<>(run, HttpStatus.CREATED);
     }
